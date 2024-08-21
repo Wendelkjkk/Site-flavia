54d8cc72914dbf546839a46cb8519e4ed9fd6b59
Wendel <105987208+Wendelkjkk@users.noreply.github.com>
2024-08-21 11:07:06 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+function enviarMensagem() {
+    const usuarioInstagram = "flavia0xz"; // Nome de usuário do Instagram sem '@'
+    const linkInstagram = `https://www.instagram.com/${usuarioInstagram}/`; // Link para o perfil do Instagram
+    
+    // Abrir o perfil do Instagram do usuário em uma nova aba
+    window.open(linkInstagram, '_blank');
+}
